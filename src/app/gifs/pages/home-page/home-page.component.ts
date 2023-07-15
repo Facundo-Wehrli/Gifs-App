033a8fd21ba7b22d737fe6b41e0fd1cdb60f5129
Facundo <wehrlifacundo9@gmail.com>
2023-07-14 21:06:42 -0300

    feat: homepage with searchbox component and gifs component

---
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interfaces';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+})
+export class HomePageComponent {
+  constructor(private GifsService: GifsService) {}
+
+  get gifs(): Gif[] {
+    return this.GifsService.gifList;
+  }
+}
