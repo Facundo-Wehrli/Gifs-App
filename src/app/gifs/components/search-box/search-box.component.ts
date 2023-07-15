8147e404d9a3e09e582434a5c77a678db2a2ddaf
Facundo <wehrlifacundo9@gmail.com>
2023-07-14 21:04:08 -0300

    feat: add search box

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gif-search-box',
+  template: `<h5>Buscar:</h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar Gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    /> `,
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifService: GifsService) {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
+}
