72cbe748cf0ac71a69db09c570e37921309604be
Facundo <wehrlifacundo9@gmail.com>
2023-07-14 21:00:04 -0300

    add: gif module

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifsCardComponent } from './components/cards/card.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+@NgModule({
+  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent, GifsCardComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [HomePageComponent, SearchBoxComponent, CardListComponent],
+})
+export class GifsModule {}
